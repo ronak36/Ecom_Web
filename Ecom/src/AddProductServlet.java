bf0d3b96a63c1cb7c99db4609cdf979b51b6c8e8
ronak36 <ronak.classified@gmail.com>
2016-09-24 18:41:30 -0500

    added Ecom project

---
+++ Ecom/src/AddProductServlet.java
@@ -0,0 +1,30 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddProductServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();  
+		String productId = request.getParameter("productId").toString();
+		String productName = request.getParameter("productName").toString();
+		String productPrice = request.getParameter("productPrice").toString();
+		String category = request.getParameter("category2").toString();
+		
+		out.println(" " + productId + " " + productName + " " + productPrice + " " + category);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Ecom/src/HomeServlet.java
@@ -0,0 +1,50 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HomeServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();  
+		String uname = request.getParameter("inputEmail").toString();
+		String password = request.getParameter("inputPassword").toString();
+		if("abc@gmail.com".equals(uname) && "abc".equals(password)){
+			out.println("Log in successful");
+			response.sendRedirect("E_Com_Web/docs/ECom_WebContent/html/dashboard.html");
+		}
+		else{
+			out.println("Log in failed");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
